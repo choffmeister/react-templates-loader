6dc304e7aeb940d8d120752755fbf054cf3f7f74
Christian Hoffmeister <mail@choffmeister.de>
2016-01-20 09:41:28 +0100

    Also parse JSON style config
    See http://webpack.github.io/docs/using-loaders.html#query-parameters.

--- index.js
+++ index.js
@@ -2,8 +2,18 @@ var reactTemplates = require('react-templates/src/reactTemplates');
 var url = require('url');
 var queryString = require('querystring');
 
+var parseOptions = function (query) {
+	try {
+		// try to parse ?{"a":"b","c":["d"]}
+		return JSON.parse(query.substr(1));
+	} catch (e) {
+		// fallback to parsing ?a=b&c=d
+		return queryString.parse(url.parse(query).query);
+	}
+};
+
 module.exports = function(source) {
-	var options = queryString.parse(url.parse(this.query).query);
+	var options = parseOptions(this.query);
 	this.cacheable && this.cacheable();
 	return reactTemplates.convertTemplateToReact(source, options);
 };
